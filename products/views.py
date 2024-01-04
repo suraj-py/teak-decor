8baea37693a63f89609730ae795768931b89a0b0
Suraj Mhatre <surajmcodes@gmail.com>
2024-01-04 18:36:30 +0530

    feat: products page update to show list of products

--- products/views.py
+++ products/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Item
 
-# Create your views here.
+def products_list(request):
+    products = Item.objects.all()
+    context = {"products": products}
+    return render(request, "products.html", context)
