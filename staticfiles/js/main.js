272a51eb41df206abe1ff229b3c871b3bec8bbe8
Suraj Mhatre <surajmcodes@gmail.com>
2024-01-12 21:33:37 +0530

    chore: made style changes in navbar

--- staticfiles/js/main.js
+++ staticfiles/js/main.js
@@ -1,7 +1,15 @@
 const menuBtn = document.querySelector('.menu-btn')
 const sideBar = document.querySelector('.sidemenu')
 
+const accountsBtn = document.querySelector('.accBtn')
+const dropdownMenu = document.querySelector('.dropdown')
+
 menuBtn.addEventListener('click', () => {
     console.log('working')
-    sideBar.classList.toggle('open')
+    sideBar.classList.toggle('hide')
+})
+
+accountsBtn.addEventListener('click', () => {
+    console.log('working')
+    dropdownMenu.classList.toggle('show')
 })
