76eccdfe1694032a0e7309aea62ac10af5559f32
Suraj Mhatre <surajmcodes@gmail.com>
2024-01-13 13:31:00 +0530

    feat: configured aws cloudfront with s3 to cache images

--- config/settings.py
+++ config/settings.py
@@ -168,7 +168,10 @@ MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+AWS_S3_CUSTOM_DOMAIN = 'd1kzjq66iwuksu.cloudfront.net'
+
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
 
 
 LOGIN_REDIRECT_URL = 'home'
