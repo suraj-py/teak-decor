f8b4bc4c93567808b0b46105c0269cffaa8ca132
Suraj Mhatre <surajmcodes@gmail.com>
2024-01-16 17:45:23 +0530

    chore: updated all UI changes

--- static/js/main.js
+++ static/js/main.js
@@ -13,3 +13,17 @@ accountsBtn.addEventListener('click', () => {
     console.log('working')
     dropdownMenu.classList.toggle('show')
 })
+
+document.addEventListener("DOMContentLoaded", function() {
+  var navbar = document.getElementById("navbar");
+  var initialHeight = 112; // Set your initial height
+
+  window.addEventListener("scroll", function() {
+    if (window.scrollY > 100) { // Adjust the scroll position where you want the change to occur
+      navbar.style.height = "80px"; // Set the new height on scroll
+    } else {
+      navbar.style.height = initialHeight + "px"; // Set the initial height if not scrolled enough
+    }
+  });
+});
+
--- theme/static_src/tailwind.config.js
+++ theme/static_src/tailwind.config.js
@@ -56,13 +56,6 @@ module.exports = {
                 'cancel-color': '#da2c38',
             },
 
-            backgroundImage: {
-                'hero-image-one': "url('https://d1kzjq66iwuksu.cloudfront.net/home_page/home1.jpeg')",
-                'hero-image-two': "url('https://d1kzjq66iwuksu.cloudfront.net/home_page/home2.jpeg')",
-                'hero-image-three': "url('https://d1kzjq66iwuksu.cloudfront.net/home_page/home3.jpeg')",
-                'hero-image-four': "url('https://d1kzjq66iwuksu.cloudfront.net/home_page/home4.jpeg')",
-            },
-
             fontFamily: {
                 merri: ['Merriweather', 'serif'],
                 roboto: ['Roboto', 'sans-serif'],
